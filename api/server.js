8a0d55207e4a17d02f8e0c95f2324e39a42ee31d
Mack Gallagher <wingsonthebus@gmail.com>
2022-06-30 15:22:38 -0500

    add HTML for Heroku to display in the browser on index route

--- api/server.js
+++ api/server.js
@@ -11,4 +11,8 @@ server.use(cors());
 server.use('/api/auth', auth_router);
 server.use('/api/game', game_router);
 
+server.get('/', (req, res) => {
+  res.status(200).send('<h1>Hello! This is the SPX Chess API. Use an instance of the client to play!</h1>');
+});
+
 module.exports = server;
