4283a93c1d0d5779e1f5cc2d5cd30c0500df5110
Mack Gallagher <wingsonthebus@gmail.com>
2022-06-30 15:08:19 -0500

    pull first-choice port from environment varibale for Heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./api/server');
 
-const PORT = 9000;
+const PORT = process.env.port || 9000;
 
 server.listen(PORT, () => {
   console.log(`server now listening on port ${PORT}!`);
