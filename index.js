a7c44a7895fbce17a901d1833e82c7f8560f9732
Mack Gallagher <wingsonthebus@gmail.com>
2022-06-30 15:19:47 -0500

    change port to PORT?

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const server = require('./api/server');
 
-const PORT = process.env.port || 9000;
+const PORT = process.env.PORT || 9000;
 
 server.listen(PORT, () => {
   console.log(`server now listening on port ${PORT}!`);
